6856251ba4db3d8d11f495cdab6d4e318bb6ce82
CasonJuu <jascuu2@gmail.com>
2025-10-11 10:02:48 -0600

    Update player_movement.gd
    reduced movementSpeed from 200 > 30
    Replaced using position attribute to using velocity and move_and_slide()

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-@export var movementSpeed := 200
+@export var movementSpeed := 30
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +11,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	position += direction * movementSpeed * delta
+	velocity = direction * movementSpeed
+	move_and_slide()
