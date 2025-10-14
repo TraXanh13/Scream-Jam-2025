4608012467179cd4c3e58aead6fabc9998766e78
CasonJuu <jascuu2@gmail.com>
2025-10-13 19:20:20 -0600

    Updated projectile
    projectile.tscn
    - added a duration timer, deletes projectile scene on timeout
    
    projectile.gd
    - added code to damage an enemy (Area2D)

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 var targetDirection
 @export var projectileSpeed := 50
+@export var duration := 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Timer.wait_time = duration
+	$Timer.start()
 	var mousePosition = get_global_mouse_position()
 	targetDirection = global_position.direction_to(mousePosition)
 
@@ -12,3 +15,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position += targetDirection * projectileSpeed * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("projectile hit!")
+	if (area != null and area.health != null):
+		area.health -= 1
+	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
